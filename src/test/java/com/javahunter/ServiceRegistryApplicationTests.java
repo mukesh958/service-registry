1fd9777a51d43645d9bf24419917403c37cb9406
unknown <mukesh958@gmail.com>
2026-01-07 10:54:31 +0530

    Fi

---
+++ src/test/java/com/javahunter/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javahunter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
